d428832d8e92674f3feeb6f7ad42c2335af7c209
supchyan <mikhailsupov@yandex.ru>
2023-10-24 01:15:57 +0300

    Alpha removed

--- Code/DontDieBuff.cs
+++ Code/DontDieBuff.cs
@@ -12,16 +12,11 @@ namespace EvenAltF4.Code {
 		public override void Update(Player player, ref int buffIndex) { // This method gets called every frame your buff is active on your player.
             player.immune = true;
             player.immuneAlpha = 1;
-            if(player.buffTime[buffIndex]==0) {
-                player.opacityForAnimation = 1f;
-            } else {
-                player.opacityForAnimation = 0.5f;
-                int time = player.buffTime[buffIndex];
-                Color col = new Color(255, 255, 2*time);
-                float scl = 1.3f*(time/120f);
-                Dust barier = Dust.NewDustPerfect(player.Center + new Vector2(0, -50f) + new Vector2(0f, -7f).RotatedBy(MathHelper.ToRadians(-6f*time)), DustID.PortalBolt, Vector2.Zero, 1, col, scl);
-                barier.noGravity = true;
-            }
+            int time = player.buffTime[buffIndex];
+            Color col = new Color(255, 255, 2*time);
+            float scl = 1.3f*(time/120f);
+            Dust barier = Dust.NewDustPerfect(player.Center + new Vector2(0, -50f) + new Vector2(0f, -7f).RotatedBy(MathHelper.ToRadians(-6f*time)), DustID.PortalBolt, Vector2.Zero, 1, col, scl);
+            barier.noGravity = true;
         }
     }
 }
